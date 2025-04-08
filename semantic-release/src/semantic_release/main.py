0f2cfce3c9e403249e6e450595fd832b3453c4e9
Viresh Soedhwa <vsoedhwa@bcit.ca>
2025-04-08 00:06:15 -0700

    init function setup

--- semantic-release/src/semantic_release/main.py
+++ semantic-release/src/semantic_release/main.py
@@ -1,63 +1,47 @@
 from typing import Annotated, Self
 from dagger import Container, dag, Directory, Doc, field, function, object_type, Secret, DefaultPath
 
+from .releaserc import ReleaseRC
+from enum import Enum
+from typing import Optional
+import json
+
+class ReleaseMode(Enum):
+    LOCAL = "local"
+    CI = "ci"
+
+
 @object_type
 class SemanticRelease:
-    # github_token: Annotated[Secret, Doc("GitHub Token")]
-    # source: Annotated[Directory, DefaultPath("./")]
-    # branch: Annotated[str, Doc("GitHub branch name")]
-    # username: Annotated[str, Doc("GitHub username")]
-    # releaserc: Annotated[str, Doc("Release configuration json")]
-    # container: Annotated[
-    #     Container,
-    #     Doc("Container to run commands in"),
-    # ] = field()
-    # @classmethod
-    # async def init(
-    #     cls,
-    #     token: Annotated[Secret, Doc("The Github Token to use")],
-    #     source: Annotated[Directory, DefaultPath("./"), Doc("The source directory")],
-    #     branch: Annotated[str, Doc("The GitHub branch name")],
-    #     username: Annotated[str, Doc("The GitHub username")]
-    #     ) -> Self:
-    #     self = cls(github_token = token, \
-    #                source = source, \
-    #                branch = branch, \
-    #                 username = username)
-    #     return cls(
-    #         container=(
-    #             dag.container()
-    #             .from_("ghcr.io/bcit-ltc/semantic-release:latest")
-    #         )
-    #     )
+    def __init__(
+        self,
+        github_token: Optional[str] = None,
+        source: Optional[str] = None,
+        branch: Optional[str] = None,
+        username: Optional[str] = None,
+        ) -> None:
+        self.mode = ReleaseMode.CI if github_token else ReleaseMode.LOCAL
 
-    @function
-    async def version(self) -> str:
-        """Returns the string argument provided"""
-        return await(
-            dag.container()
-            .from_("ghcr.io/bcit-ltc/semantic-release:latest")
-            .with_workdir("/app") 
-            .with_new_file(
-            "/app/hello.txt",
-            contents=f"""Helloo World""",
-            ).with_exec(["cat", "/app/hello.txt"])
-            .stdout()
-        )
+        if self.mode == ReleaseMode.CI:
+            if not all([source, branch, username]):
+                raise ValueError("CI mode requires: source, branch, username")
+
+        self.github_token = github_token
+        self.source = source
+        self.branch = branch
+        self.username = username
+        self.releaserc = ReleaseRC() # Initialize with an empty config
+        self.container: Optional[Container] = None
+        self.exec_output_file = "NEXT_VERSION"  # Store the exec output file variable
+
+        self._set_required_plugins() # Set required plugins based on the mode CI or LOCAL
+        self.releaserc.set("branches", [branch])
+        self.releaserc.set("dryRun", False)
+        self.releaserc.set("ci", True if self.mode == ReleaseMode.CI else False)
+        self.releaserc.set("debug", True)
+        self.releaserc.set("tagFormat", "${{version}}")
+
+        # print(self.releaserc.to_dict())  # Debugging line
+        print(json.dumps(self.releaserc.to_dict(), indent=2))  # Debugging line
+
         
-    @function
-    def env(self) -> Container:
-        """Returns a container with the necessary environment for git and gh"""
-        return (
-            dag.container() \
-            .from_("ghcr.io/bcit-ltc/semantic-release:latest") \
-            .with_workdir("/app") \
-            .with_directory("/app", self.source) \
-            .with_secret_variable("GITHUB_TOKEN", self.github_token) \
-            .with_exec(["cp", "/usr/src/app/.releaserc", "/app/.releaserc"]) \
-            .with_exec(["cat", "/app/.releaserc"]) \
-            .with_exec(["npx", "semantic-release", "--branches", self.branch]) \
-            .with_exec(["ls", "-la"]) \
-            .with_exec(["cat", "NEXT_VERSION"]) \
-            .stdout()
-        )
