5cc2e70898791ae9cad9d5753093ed16aba7b3a1
Viresh Soedhwa <vsoedhwa@bcit.ca>
2025-04-13 00:30:43 -0700

    ci provider configs

--- semantic-release/src/semantic_release/main.py
+++ semantic-release/src/semantic_release/main.py
@@ -19,12 +19,24 @@ project, maintained by its contributors. All semantic-release core functionaliti
 """
 
 from typing import Annotated, Self
-from dagger import Container, dag, Directory, Doc, field, function, object_type, Secret, DefaultPath
+from dagger import Container, dag, Directory, Doc, field, function, object_type, Secret, DefaultPath, enum_type
+
+from enum import Enum
 
 from .releaserc import ReleaseRC
+import os
+
+@enum_type
+class CiProvider(Enum):
+    NONE = "none"
+    UNKNOWN = "unknown"
+    GITHUB = "github"
+
+
 
 @object_type
 class SemanticRelease:
+    releaserc = ReleaseRC()
     @function
     async def run(self,
             source: Annotated[Directory, Doc("Source directory"), DefaultPath(".")], # source directory
@@ -32,28 +44,52 @@ class SemanticRelease:
             username: Annotated[str, Doc("Github Username")],  # GitHub username with default value
             branch: Annotated[str, Doc("Branch")] = "main",  # Default branch name
             ) -> None:
-        releaserc = ReleaseRC()
-        releaserc.add_branch(branch)
-        releaserc.add_plugin("@semantic-release/commit-analyzer")
-        releaserc.add_plugin("@semantic-release/release-notes-generator")
-        releaserc.set_dry_run(True)
-        releaserc.set_ci(True)
-        releaserc.set_debug(True)
-        # print(releaserc.to_string())
+        
+        if os.getenv("CI") == "true":
+            print("Running in CI")
+            self.ci_provider = CiProvider.CI
+            if os.getenv("GITHUB_ACTIONS") == "true":
+                print("Running in GitHub Actions")
+                self.ci_provider = CiProvider.GITHUB
+        else:
+            print("Running locally")
+            self.ci_provider = CiProvider.NONE
 
+        self.branch = branch
+        self.username = username
+        self.github_token = github_token
+
+        # Configure release parameters based on the CI provider
+        self._configure_release_params()
+        print(f"Configured release parameters: {self.releaserc.to_string()}")
+        
         # Create a container for running semantic release
         container = await self.semantic_release_container(source)
         # Set environment variables for the container
         await container.with_new_file(
-            "app/.releaserc", contents=releaserc.to_string()
+            ".releaserc", contents=self.releaserc.to_string()
         ).with_exec(
             ["ls", "-la"]
         ).stdout()
+
+
+    def _configure_release_params(self):
+        self.releaserc.add_branch(self.branch)
+        self.releaserc.add_plugin("@semantic-release/commit-analyzer")
+        self.releaserc.add_plugin("@semantic-release/release-notes-generator")
+    
+        """Configure release parameters based on the CI provider."""
+        if self.ci_provider == CiProvider.GITHUB:
+            self.releaserc.add_plugin("@semantic-release/github")
+            self.releaserc.set_ci(True)
+        else:
+            print("No CI provider detected. Skipping GitHub configuration.")
+            self.releaserc.set_dry_run(True)
+            self.releaserc.set_debug(True)
+
 
     async def semantic_release_container(self, source: Directory) -> Container:
         """Get the container for running semantic release."""
         return await dag.container().from_("alpine:latest").with_directory(
             "/app", source
         ).with_workdir("/app")
-    
-    
