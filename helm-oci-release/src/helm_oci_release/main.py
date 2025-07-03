e506e8a2a4e5e631b754fd4a1dbda36ef5063e21
Viresh Soedhwa <vsoedhwa@bcit.ca>
2025-07-03 09:10:20 -0700

    run function added

--- helm-oci-release/src/helm_oci_release/main.py
+++ helm-oci-release/src/helm_oci_release/main.py
@@ -1,22 +1,14 @@
 import dagger
-from dagger import dag, function, object_type
-
+from dagger import dag, function, object_type, DefaultPath, Directory, Doc
+from typing import Annotated
 
 @object_type
 class HelmOciRelease:
-    @function
-    def container_echo(self, string_arg: str) -> dagger.Container:
-        """Returns a container that echoes whatever string argument is provided"""
-        return dag.container().from_("alpine:latest").with_exec(["echo", string_arg])
+
 
     @function
-    async def grep_dir(self, directory_arg: dagger.Directory, pattern: str) -> str:
-        """Returns lines that match a pattern in the files of the provided Directory"""
-        return await (
-            dag.container()
-            .from_("alpine:latest")
-            .with_mounted_directory("/mnt", directory_arg)
-            .with_workdir("/mnt")
-            .with_exec(["grep", "-R", pattern, "."])
-            .stdout()
-        )
+    async def run(self,
+            source: Annotated[Directory, Doc("Source directory"), DefaultPath(".")], # source directory
+            ) -> str:
+
+        return "Helm OCI Release"
