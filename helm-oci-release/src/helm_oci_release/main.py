e3a5bdb7a1a5c5efeb1b958005f00fff1bf317bf
Viresh Soedhwa <vsoedhwa@bcit.ca>
2025-07-03 13:01:26 -0700

    list directory content

--- helm-oci-release/src/helm_oci_release/main.py
+++ helm-oci-release/src/helm_oci_release/main.py
@@ -1,14 +1,46 @@
 import dagger
-from dagger import dag, function, object_type, DefaultPath, Directory, Doc
+from dagger import dag, function, object_type, DefaultPath, Directory, Doc, Secret, Container
 from typing import Annotated
+
+HELM_IMAGE = "alpine/helm:3.12.0"
+WORKDIR = "/app"
 
 @object_type
 class HelmOciRelease:
-
+    helm_container = None
 
     @function
     async def run(self,
             source: Annotated[Directory, Doc("Source directory"), DefaultPath(".")], # source directory
             ) -> str:
 
-        return "Helm OCI Release"
+        await self._prepare_helm_container(source)
+        await self._setup_helm_directory(source)
+
+        return None
+
+    async def _setup_helm_directory(self, source: Directory) -> Directory:
+        """
+        Set up the Helm directory structure in the container.
+        """
+        
+        result = await self.helm_container.with_directory(
+            WORKDIR, source
+        ).with_workdir(WORKDIR).with_exec(
+            ["ls", "-la"])
+
+        return await result.stdout()
+    
+
+
+    async def _prepare_helm_container(self, source: Directory) -> Container:
+        self.helm_container =  await dag.container().from_(HELM_IMAGE)
+        return self.helm_container
+    
+    # async def _run_helm_command(self, command: str, args: list[str]) -> str:
+    #     """
+    #     Run a Helm command with the given arguments.
+    #     """
+    #     container = await self._prepare_helm_container()
+    #     result = await container.with_exec(["helm", command] + args).stdout()
+    #     return result
