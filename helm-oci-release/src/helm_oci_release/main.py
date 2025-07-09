00d70450be33f6bd2ded0e67da6d6b86a282a6df
Viresh Soedhwa <vsoedhwa@bcit.ca>
2025-07-09 11:32:16 -0700

    test

--- helm-oci-release/src/helm_oci_release/main.py
+++ helm-oci-release/src/helm_oci_release/main.py
@@ -71,7 +71,7 @@ class HelmOciRelease:
         temp_path = workdir / helm_directory_path
         final_path = str(temp_path)
         print(f"[DEBUG] Setting Helm workdir to: {final_path}")
-        return await container.with_workdir(self.appname)
+        return await container.with_workdir(self.appname + "/" + helm_directory_path )
 
     async def add_ghcr_password_secret(self, container: Container, github_token: Secret) -> Container:
         """
